fb68dfdb6eea2a5e8164a98b0cd4822ce7a64769
tanmoy <tanmoymandal457@gmail.com>
2020-02-11 01:42:04 +0530

    bmi calculator complete

---
+++ lib/constants.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+const kCardColor = Color(0xFF1D1F33);
+const kBottomColor = Color(0xFFEB1555);
+const kinactivecardcolor= Color(0xFF111328);
+
+
+const klabelTextStyle =  TextStyle(
+  fontSize: 18.0, color: Color(0xFF8D8E98),
+);
+
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kresultTextStyle= TextStyle(
+  color: Color(0xFF24D876),
+  fontWeight: FontWeight.bold,
+  fontSize: 22.0,
+);
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+const kBodyTextStyle = TextStyle (
+  fontSize: 20.0,
+
+);
